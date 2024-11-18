cc56cadd72da3c8ffafa1f30a87d8b75b2cccb9d
tungtokyo1108 <dangthanhtung91@vn-bml.com>
2024-11-18 18:10:01 +0900

    Update Test_VBayesMM_pytorch.py

--- scripts/Test_VBayesMM_pytorch.py
+++ scripts/Test_VBayesMM_pytorch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 13 17:08:19 2024
 
 @author: tungbioinfo
 """
@@ -32,14 +31,14 @@ from VBayesMM import VBayesMM
 
 microbes = load_table("microbes.biom")
 metabolites = load_table("metabolites.biom")
-microbes_df = microbes.to_dataframe().T
-metabolites_df = metabolites.to_dataframe().T
+microbes_df = microbes.to_dataframe()
+metabolites_df = metabolites.to_dataframe()
 
 microbes_df = microbes_df.astype(pd.SparseDtype("float64",fill_value=0))
 metabolites_df = metabolites_df.astype(pd.SparseDtype("float64",fill_value=0))
 microbes_df, metabolites_df = microbes_df.align(metabolites_df, axis=0, join='inner')
 
-num_test = 5
+num_test = 20
 
 sample_ids = set(np.random.choice(microbes_df.index, size=num_test))
 sample_ids = np.array([(x in sample_ids) for x in microbes_df.index])
@@ -63,11 +62,11 @@ testY_torch = torch.tensor(test_metabolites_df.to_numpy(), dtype=torch.float32)
 model = VBayesMM(d1=d1, d2=d2, num_samples=n, batch_size=n, device='cpu', 
                  subsample_size = 10, latent_dim=3,
                  unorm_type = 2.0, vnorm_type = 0, 
-                temperature = 0.5, mu01 = 0, mu02 = 1, rho01=0, rho02=1, lambda01=0, lambda02=1, ssprior = "normal", 
-     #            temperature = 0.5, mu01 = -0.6, mu02 = 0.6, rho01=-6., rho02=-6., lambda01=0.99, lambda02=0.99, ssprior = "uniform",
+     #           temperature = 0.5, mu01 = 0, mu02 = 1, rho01=0, rho02=1, lambda01=0, lambda02=1, ssprior = "normal", 
+                 temperature = 0.5, mu01 = -0.6, mu02 = 0.6, rho01=-6., rho02=-6., lambda01=0.99, lambda02=0.99, ssprior = "uniform",
                  hard=False, threshold = 0.5)
 
-losses, val_losses = model.fit(train_microbes_coo, trainY_torch, test_microbes_coo, testY_torch, epochs=200)
+losses, val_losses = model.fit(train_microbes_coo, trainY_torch, test_microbes_coo, testY_torch, epochs=5000)
 
 plt.plot(losses, "green")
 
