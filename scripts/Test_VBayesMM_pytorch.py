9569ddc612d87a3d9645a8dbd1c6eaed0a7d662d
tungtokyo1108 <dangthanhtung91@vn-bml.com>
2024-11-27 17:14:21 +0900

    Update Test_VBayesMM_pytorch.py

--- scripts/Test_VBayesMM_pytorch.py
+++ scripts/Test_VBayesMM_pytorch.py
@@ -22,7 +22,7 @@ from skbio.stats.composition import clr, centralize, clr_inv
 from scipy.sparse import coo_matrix
 
 import torch
-from VBayesMM import VBayesMM
+from VBayesMM_v1 import VBayesMM
 
 
 #------------------------------------------------------------------------------
@@ -59,37 +59,82 @@ testY_torch = torch.tensor(test_metabolites_df.to_numpy(), dtype=torch.float32)
 
 # Initialize the VBayesMM model
 
-model = VBayesMM(d1=d1, d2=d2, num_samples=n, batch_size=n, device='cpu', 
-                 subsample_size = 10, latent_dim=3,
-                 unorm_type = 2.0, vnorm_type = 0, 
-     #           temperature = 0.5, mu01 = 0, mu02 = 1, rho01=0, rho02=1, lambda01=0, lambda02=1, ssprior = "normal", 
-                 temperature = 0.5, mu01 = -0.6, mu02 = 0.6, rho01=-6., rho02=-6., lambda01=0.99, lambda02=0.99, ssprior = "uniform",
-                 hard=False, threshold = 0.5)
+model = VBayesMM(d1=d1, d2=d2, num_samples=n, batch_size=n) 
 
 losses, val_losses, val_SMAPE = model.fit(train_microbes_coo, trainY_torch, test_microbes_coo, testY_torch, epochs=5000)
 
 plt.plot(losses, "green")
+plt.xlabel('Iteration', size=15)
+plt.ylabel('ELBO', size=15)
 
 plt.plot(val_losses, "red")
 
-# Microbiome U and metabolite V matrices 
+plt.plot(val_SMAPE, "red")
+plt.xlabel('Iteration', size=15)
+plt.ylabel('SMAPE', size=15)
+
+# Microbiome U and metabolite V matrices plt.plot(val_SMAPE, "red")
 
 u_mean = np.array(model.qUmain_mean.weight.data.detach())
-u_bias = np.array(model.qUbias_mean.weight.data.detach())
-
 plt.figure(figsize=(6, 6))
 sns.histplot(np.ravel(u_mean), bins=50, kde=True, color='red')
 
-def kl_div(mu, log_var):
-    return 1 + log_var - mu**2 - np.exp(log_var)
-
-u_mean_mu = np.ravel(model.qUmain_mean.weight.data.detach())
-u_mean_logvar = np.ravel(model.qUmain_std.weight.data.detach())
-plt.figure(figsize=(6, 6))
-sns.histplot(kl_div(u_mean_mu, u_mean_logvar), bins=50, kde=True, color='red')
-
 u_gamma_mean = np.array(model.qUmain_mean_gamma.detach())
 Umain_mean_gamma_mean = np.sort(np.mean(u_gamma_mean, axis=1))[::-1]
-
 plt.figure(figsize=(6, 6))
 sns.histplot(Umain_mean_gamma_mean , bins=50, kde=True, color='red', stat="count", alpha=0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
