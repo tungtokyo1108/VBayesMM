6478c5dd7ae1eed1023ca1ba78176ceef1304302
tungtokyo1108 <dangthanhtung91@vn-bml.com>
2025-04-09 15:01:39 +0900

    Update VBayesMM.py

--- src/tensorflow/VBayesMM.py
+++ src/tensorflow/VBayesMM.py
@@ -34,11 +34,12 @@ def logit(z):
     return tf.math.log(z/(1. - z))
 
 def sample_gumbel(shape, eps=1e-8):
-    U = tf.random.uniform(shape, minval=0, maxval=1)
-    return -tf.math.log(-tf.math.log(U + eps) + eps)
+    
+    uniform = tf.random.uniform(shape, minval=0, maxval=1)
 
-    #U = tf.random.uniform(shape, minval=0, maxval=1)
-    #return tf.math.log(U + eps) - tf.math.log(1.0 - U + eps)
+    # Clip uniform samples for numerical stability
+    uniform = tf.clip_by_value(uniform, eps, 1.0 - eps)
+    return -tf.math.log(-tf.math.log(uniform))
 
 def gumbel_softmax(logits, temperature=0.5, hard=False):
     
@@ -111,6 +112,9 @@ class VBayesMM(object):
     def __init__(self, batch_size=50, latent_dim=3, learning_rate=0.1, v_mean=0, v_scale=1,
                  temperature = 0.5, mu01 = 0, mu02 = 1, rho01=0, rho02=1, lambda01=0, lambda02=0.3, 
                  hard = False, threshold = 0.5, 
+                 dist_type="laplace",  # Options: "normal", "student_t", "laplace", "mixture"
+                 df=4.0,  # Degrees of freedom for Student's t
+                 scale=1.0,  # Scale parameter for Laplace
                  clipnorm=10., device_name='/cpu:0', save_path=None):
         
         
@@ -144,8 +148,28 @@ class VBayesMM(object):
         self.v_mean = v_mean
         self.v_scale = v_scale
         
+        # Distribution parameters
+        self.dist_type = dist_type
+        self.df = df
+        self.scale = scale
+        
         self.clipnorm = clipnorm
         self.save_path = save_path
+    
+    def sample_from_distribution(self, shape, mean, scale, dist_type):
+        """Sample from different distributions based on dist_type"""
+        if dist_type == "normal":
+            return tf.random.normal(shape, mean=mean, stddev=scale)
+        elif dist_type == "student_t":
+            # Student's t-distribution with location and scale
+            return mean + scale * tf.random.normal(shape) / tf.sqrt(
+                tf.random.gamma(shape, alpha=self.df/2, beta=self.df/2))
+        elif dist_type == "laplace":
+            # Laplace distribution
+            u = tf.random.uniform(shape, minval=-0.5, maxval=0.5)
+            return mean - scale * tf.sign(u) * tf.math.log(1 - 2 * tf.abs(u))
+        else:
+            raise ValueError(f"Unknown distribution type: {dist_type}")
 
     def __call__(self, session, trainX, trainY, testX, testY):
         
@@ -210,14 +234,56 @@ class VBayesMM(object):
             
             # Spike-and-slab variational prior
             
-            self.qUmain_mean_w_mu = tf.Variable(tf.random.normal(shape=[self.d1, self.p], mean=self.mu01, stddev=self.mu02), name='qUmain_mean_w_mu', trainable=True)
-            self.qUmain_mean_w_rho = tf.Variable(tf.random.normal(shape=[self.d1, self.p], mean=self.rho01, stddev=self.rho02), name='qUmain_mean_w_rho', trainable=True)
-            self.qUmain_mean_w_theta = tf.Variable(logit(tf.random.uniform(shape=[self.d1, self.p], minval=self.lambda01, maxval=self.lambda02)), name='qUmain_mean_w_theta', trainable=True)
+            self.qUmain_mean_w_mu = tf.Variable(
+                self.sample_from_distribution(
+                    shape=[self.d1, self.p],
+                    mean=self.mu01,
+                    scale=self.mu02,
+                    dist_type=self.dist_type
+                ),
+                name='qUmain_mean_w_mu',
+                trainable=True)
+            self.qUmain_mean_w_rho = tf.Variable(
+                self.sample_from_distribution(
+                    shape=[self.d1, self.p],
+                    mean=self.rho01,
+                    scale=self.rho02,
+                    dist_type=self.dist_type
+                ),
+                name='qUmain_mean_w_rho',
+                trainable=True)
+            self.qUmain_mean_w_theta = tf.Variable(
+                logit(tf.random.uniform(shape=[self.d1, self.p], 
+                                        minval=self.lambda01, 
+                                        maxval=self.lambda02)), 
+                name='qUmain_mean_w_theta', 
+                trainable=True)
             self.qUmain_mean_gamma = None
-                
-            self.qUbias_mean_w_mu = tf.Variable(tf.random.normal(shape=[self.d1, 1], mean=self.mu01, stddev=self.mu02), name='qUbias_mean_w_mu', trainable=True)
-            self.qUbias_mean_w_rho = tf.Variable(tf.random.normal(shape=[self.d1, 1], mean=self.rho01, stddev=self.rho02), name='qUbias_mean_w_rho', trainable=True)
-            self.qUbias_mean_w_theta = tf.Variable(logit(tf.random.uniform(shape=[self.d1, 1], minval=self.lambda01, maxval=self.lambda02)), name='qUbias_mean_w_theta', trainable=True)
+            
+            self.qUbias_mean_w_mu = tf.Variable(
+                self.sample_from_distribution(
+                    shape=[self.d1, 1],
+                    mean=self.mu01,
+                    scale=self.mu02,
+                    dist_type=self.dist_type
+                ),
+                name='qUbias_mean_w_mu',
+                trainable=True)
+            self.qUbias_mean_w_rho = tf.Variable(
+                self.sample_from_distribution(
+                    shape=[self.d1, 1],
+                    mean=self.rho01,
+                    scale=self.rho02,
+                    dist_type=self.dist_type
+                ),
+                name='qUbias_mean_w_rho',
+                trainable=True)
+            self.qUbias_mean_w_theta = tf.Variable(
+                logit(tf.random.uniform(shape=[self.d1, 1], 
+                                        minval=self.lambda01, 
+                                        maxval=self.lambda02)), 
+                name='qUbias_mean_w_theta', 
+                trainable=True)
             self.qUbias_mean_gamma = None
             
             
@@ -227,27 +293,52 @@ class VBayesMM(object):
             qUbias_mean_sigma_w = tf.math.softplus(self.qUbias_mean_w_rho, name="qUbias_mean_sigma_w")
             sigma_prior = tf.math.softplus(self.rho_prior, name="sigma_prior")
             
+            # Clip variance to prevent extreme values
+            qUmain_mean_sigma_w = tf.clip_by_value(qUmain_mean_sigma_w, 0.01, 1.0)
+            qUbias_mean_sigma_w = tf.clip_by_value(qUbias_mean_sigma_w, 0.01, 1.0)
+            
             qUmain_mean_u_w = tf.random.uniform(shape=self.qUmain_mean_w_theta.shape, minval=0, maxval=1)
             qUbias_mean_u_w = tf.random.uniform(shape=self.qUbias_mean_w_theta.shape, minval=0, maxval=1)
             
             self.qUmain_mean_gamma = gumbel_softmax(self.qUmain_mean_w_theta, temperature=self.temp, hard=self.hard)
             self.qUbias_mean_gamma = gumbel_softmax(self.qUbias_mean_w_theta, temperature=self.temp, hard=self.hard)
             
-            qUmain_mean_epsilon_w = tf.random.normal(shape=self.qUmain_mean_w_mu.shape, mean=0, stddev=0.001)
-            qUbias_mean_epsilon_w = tf.random.normal(shape=self.qUbias_mean_w_mu.shape, mean=0, stddev=0.001)
+            qUmain_mean_epsilon_w = tf.random.truncated_normal(
+                shape=self.qUmain_mean_w_mu.shape, 
+                mean=0, 
+                stddev=0.001)
+            qUbias_mean_epsilon_w = tf.random.truncated_normal(
+                shape=self.qUbias_mean_w_mu.shape, 
+                mean=0, 
+                stddev=0.001)
             
             # Variational distributions for V matrix
             
-            self.qVmain_mean = tf.Variable(tf.random.normal([self.p, self.d2-1]), name='qVmain_mean', trainable=True)
-            self.qVmain_std = tf.Variable(tf.random.normal([self.p, self.d2-1]), name='qVmain_std', trainable=True)
-            self.qVbias_mean = tf.Variable(tf.random.normal([1, self.d2-1]), name='qVbias_mean', trainable=True)
-            self.qVbias_std = tf.Variable(tf.random.normal([1, self.d2-1]), name='qVbias_std', trainable=True)
+            self.qVmain_mean = tf.Variable(
+                tf.random.normal([self.p, self.d2-1]), 
+                name='qVmain_mean', 
+                trainable=True)
+            self.qVmain_std = tf.Variable(
+                tf.random.normal([self.p, self.d2-1]), 
+                name='qVmain_std', 
+                trainable=True)
+            self.qVbias_mean = tf.Variable(
+                tf.random.normal([1, self.d2-1]), 
+                name='qVbias_mean', 
+                trainable=True)
+            self.qVbias_std = tf.Variable(
+                tf.random.normal([1, self.d2-1]), 
+                name='qVbias_std', 
+                trainable=True)
             
+            # Clip standard deviations for stability
+            self.qVmain_std = tf.clip_by_value(self.qVmain_std, -5.0, 3.0)
+            self.qVbias_std = tf.clip_by_value(self.qVbias_std, -5.0, 3.0)
                 
             self.qUmain  = self.qUmain_mean_gamma * (self.qUmain_mean_w_mu + qUmain_mean_epsilon_w * qUmain_mean_sigma_w)
             self.qUbias = self.qUbias_mean_gamma * (self.qUbias_mean_w_mu + qUbias_mean_epsilon_w * qUbias_mean_sigma_w)
-            self.qVmain = self.qVmain_mean + (tf.exp(self.qVmain_std * 0.5) * tf.random.normal(shape=self.qVmain_std.shape, mean=0, stddev=0.001))
-            self.qVbias = self.qVbias_mean + (tf.exp(self.qVbias_std * 0.5) * tf.random.normal(shape=self.qVbias_std.shape, mean=0, stddev=0.001))
+            self.qVmain = self.qVmain_mean + (tf.exp(self.qVmain_std * 0.5) * tf.random.truncated_normal(shape=self.qVmain_std.shape, mean=0, stddev=0.001))
+            self.qVbias = self.qVbias_mean + (tf.exp(self.qVbias_std * 0.5) * tf.random.truncated_normal(shape=self.qVbias_std.shape, mean=0, stddev=0.001))
 
 
             qU = tf.concat(
@@ -271,21 +362,32 @@ class VBayesMM(object):
             qUmain_mean_w_phi = tf.math.sigmoid(self.qUmain_mean_w_theta, name="qUmain_mean_w_phi")
             qUbias_mean_w_phi = tf.math.sigmoid(self.qUbias_mean_w_theta, name="qUbias_mean_w_phi")
             
-            kl_Umain_mean = qUmain_mean_w_phi * (tf.math.log(qUmain_mean_w_phi) - tf.math.log(self.phi_prior)) + \
-                        (1 - qUmain_mean_w_phi) * (tf.math.log(1 - qUmain_mean_w_phi) - tf.math.log(1 - self.phi_prior)) + \
-                        qUmain_mean_w_phi * (tf.math.log(sigma_prior) - tf.math.log(qUmain_mean_sigma_w) + 
-                                                  0.5 * (qUmain_mean_sigma_w ** 2 + self.qUmain_mean_w_mu ** 2) / sigma_prior ** 2 - 0.5)
+            # Enhanced KL divergence with better numerical stability
+            kl_Umain_mean = qUmain_mean_w_phi * (
+                tf.math.log(qUmain_mean_w_phi + 1e-8) - 
+                tf.math.log(self.phi_prior + 1e-8)
+                ) + (1 - qUmain_mean_w_phi) * (
+                    tf.math.log(1 - qUmain_mean_w_phi + 1e-8) - tf.math.log(1 - self.phi_prior + 1e-8)
+                ) + qUmain_mean_w_phi * (
+                    tf.math.log(sigma_prior + 1e-8) - tf.math.log(qUmain_mean_sigma_w + 1e-8) + 
+                    0.5 * (qUmain_mean_sigma_w ** 2 + self.qUmain_mean_w_mu ** 2) / (sigma_prior ** 2 + 1e-8) - 0.5)
             kl_Umain_mean = tf.reduce_sum(kl_Umain_mean, name="kl_Umain_mean")
             
-            kl_Ubias_mean = qUbias_mean_w_phi * (tf.math.log(qUbias_mean_w_phi) - tf.math.log(self.phi_prior)) + \
-                        (1 - qUbias_mean_w_phi) * (tf.math.log(1 - qUbias_mean_w_phi) - tf.math.log(1 - self.phi_prior)) + \
-                            qUbias_mean_w_phi * (tf.math.log(sigma_prior) - tf.math.log(qUbias_mean_sigma_w) + 
-                                                 0.5 * (qUbias_mean_sigma_w ** 2 + self.qUbias_mean_w_mu ** 2) / sigma_prior ** 2 - 0.5)
+            kl_Ubias_mean = qUbias_mean_w_phi * (
+                tf.math.log(qUbias_mean_w_phi + 1e-8) - 
+                tf.math.log(self.phi_prior + 1e-8)
+                ) + (1 - qUbias_mean_w_phi) * (
+                    tf.math.log(1 - qUbias_mean_w_phi + 1e-8) - tf.math.log(1 - self.phi_prior + 1e-8)
+                ) + qUbias_mean_w_phi * (tf.math.log(sigma_prior + 1e-8) - tf.math.log(qUbias_mean_sigma_w + 1e-8) + 
+                    0.5 * (qUbias_mean_sigma_w ** 2 + self.qUbias_mean_w_mu ** 2) / (sigma_prior ** 2 + 1e-8) - 0.5)
             kl_Ubias_mean = tf.reduce_sum(kl_Ubias_mean, name="kl_Ubias_mean")
 
-            kl_Vmain = 0.5 * tf.reduce_sum(1 + self.qVmain_std - tf.square(self.qVmain_mean) - tf.exp(self.qVmain_std), name='kl_Vmain')
-            kl_Vbias = 0.5 * tf.reduce_sum(1 + self.qVbias_std - tf.square(self.qVbias_mean) - tf.exp(self.qVbias_std), name="kl_Vbias")
-            kl_divergence = kl_Vmain + kl_Vbias + kl_Umain_mean + kl_Ubias_mean 
+            # Enhanced KL divergence for V matrices with better numerical stability
+            kl_Vmain = 0.5 * tf.reduce_sum(1 + self.qVmain_std - tf.square(self.qVmain_mean) - 
+                                           tf.exp(tf.clip_by_value(self.qVmain_std, -20, 20)), name='kl_Vmain')
+            kl_Vbias = 0.5 * tf.reduce_sum(1 + self.qVbias_std - tf.square(self.qVbias_mean) - 
+                                           tf.exp(tf.clip_by_value(self.qVbias_std, -20, 20)), name="kl_Vbias")
+            kl_divergence = kl_Vmain + kl_Vbias + kl_Umain_mean + kl_Ubias_mean  
             
             self.log_loss = -((logprob_y * norm) + kl_divergence)
 
